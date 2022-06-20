68af5b5f21d2329050b05ab8adfe7af3ba9dae1c
ivanfbj <ivanfbj@gmail.com>
2022-06-19 22:50:45 -0500

    Rock Paper Scissors
    User versus computer

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 """
 source:
-https://youtu.be/8ext9G7xspg?t=414
-Guess the Number
-The objective of this project is to develop a software for Guess the number, create number for computer.
-This proyect will building with functions, loop while, conditionals and user inputs.
+https://youtu.be/8ext9G7xspg?t=1274
+https://youtu.be/8ext9G7xspg?t=1274
+The objective of this project is to develop a software for play Rock Paper and scissors, user versus computer.
 """
 import random
 
 
-def guess(number):
-    random_number = random.randint(1, number)
-    guess = 0
-    while guess != random_number:
-        guess = int(input(f'Guess a number between 1 and {number}: '))
-        if guess < random_number:
-            print('Sorry, guess again. Too low')
-        elif guess > random_number:
-            print('Sorry, guess again. Too high')
-    print(f'Yay, congrats. You have guessed the number {random_number} correctly.')
+def is_win(player, opponent):
+    # Return true if player wins
+    # r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+            or (player == 'p' and opponent == 'r'):
+        return True
+
+
+def play():
+    user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
+    computer = random.choice(['r', 'p', 's'])
+
+    if user == computer:
+        return 'It\'s a tie'
+    # r > s, s > p, p > r
+    if is_win(user, computer):
+        return 'You won!'
+    else:
+        return 'You lost!'
 
 
 if __name__ == "__main__":
-    guess(10)
+    print(play())
